c7b2b33a675ba768753636516fcf3482c2afcc13
gui-macedo-7 <guilherme.maga28@gmail.com>
2026-05-26 18:38:26 -0300

    Alterações na header, organização de pastas.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function trocarImagem(el) {
-    if (window.innerWidth < 1024) {
-        el.classList.toggle('ativa');
-    }
-}
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+const body = document.querySelector('body')
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    body.classList.toggle('scroll-disable')
+})
+
+// Fechar menu ao clicar no link
+const menuLinks = document.querySelectorAll('.menu a')
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active')
+    })
+})
